d18ea8ffb587a259949a39e1b316f710ccfdaef4
Baxtiyor <baxtiyorr545@gmail.com>
2025-05-07 08:38:48 +0500

    User Repository qo'shildi  userlarni faylga saqlashuchun

--- src/main/java/org/register/Repositories/UserRepository.java
+++ src/main/java/org/register/Repositories/UserRepository.java
@@ -9,4 +9,27 @@ import java.util.List;
 public class UserRepository {
     public static final String USER_FILE = "user.txt";
 
+    public boolean saveUser(User user) {
+        List<User> users = new ArrayList<User>();
+        users.add(user);
+        return writeUser(users);
+    }
+
+    private boolean writeUser(List<User> users) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(USER_FILE))) {
+            oos.writeObject(users);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    private List<User> readUser() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(USER_FILE))) {
+            List<User> users = (List<User>) ois.readObject();
+            return users;
+        } catch (IOException | ClassNotFoundException e) {
+            return new ArrayList<>();
+        }
+    }
 }
